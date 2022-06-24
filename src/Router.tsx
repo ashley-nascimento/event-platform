dbcd4e93bf3468d6619bbad95fea3d24bdb5c71a
Ashley <ashley_nascimento@hotmail.com>
2022-06-24 00:14:35 -0300

    adding home page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { Event } from "./pages/Event";
+import { Subscriber } from "./pages/Subscribe";
 
 export function Router(){
     return (
         <Routes>
-            <Route path="/" element={<h1>Home</h1>}></Route>
+            <Route path="/" element={<Subscriber />}></Route>
             <Route path="/event" element={<Event />}></Route>
             <Route path="/event/lesson/:slug" element={<Event />}></Route>
         </Routes>
