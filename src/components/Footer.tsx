762439b68616968454b63998e65a62d337dcae91
Ashley <ashley_nascimento@hotmail.com>
2022-06-26 22:35:01 -0300

    adding responsive homepage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { GithubLogo } from "phosphor-react";
 
 export function Footer(){
     return (
-        <footer className="bg-gray-900 border-t border-t-gray-600 py-6 px-6 flex justify-between">
+        <footer className="bg-gray-900 border-t border-t-gray-600 py-6 px-6 flex justify-center md:justify-between flex-wrap md:flex-nowrap gap-6">
             <span className="text-gray-300 block leading-tight">
                 Desenvolvido por Ashley Nascimento
             </span>
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -27,49 +27,48 @@ export function Subscriber(){
 
     return(
         <>
-                <main className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-            <img className="absolute" src={ReactIconImg} alt="React Icon" />
-            <div className="z-10 w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-                <div className="max-w-[640px]">
-                    <Logo />
-                    <h1 className="mt-8 text-[2.5rem] leading-tight">
-                        Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React JS</strong>
-                    </h1>
-                    <p className="mt-4 text-gray-200 leading-relaxed">
-                        Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
-                    </p>
+            <main className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+                <img className="absolute" src={ReactIconImg} alt="React Icon" />
+                <div className="z-10 w-full max-w-[1100px] flex-col md:flex-row flex items-center justify-between mt-20 mx-auto">
+                    <div className="md:max-w-[640px] text-center md:text-left flex flex-col items-center md:block">
+                        <Logo />
+                        <h1 className="mt-8 text-3xl md:text-[2.5rem] leading-tight">
+                            Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React JS</strong>
+                        </h1>
+                        <p className="mt-4 text-gray-200 leading-relaxed mx-6 md:mx-0 mb-8 md:mb-0">
+                            Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
+                        </p>
+                    </div>
+                    <div className="p-8 bg-gray-700 border border-gray-500 rounded w-full md:w-max">
+                        <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+                        <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
+                            <input 
+                                type="text" 
+                                className="bg-gray-900 rounded px-5 h-14" 
+                                placeholder="Seu nome completo" 
+                                required
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <input 
+                                type="email" 
+                                className="bg-gray-900 rounded px-5 h-14" 
+                                placeholder="Digite seu e-mail" 
+                                required
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <button 
+                                type="submit"
+                                disabled={loading} 
+                                className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50">
+                                    Garantir minha vaga
+                            </button>
+                        </form>
+                    </div>
                 </div>
-                <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-                    <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
-                    <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
-                        <input 
-                            type="text" 
-                            className="bg-gray-900 rounded px-5 h-14" 
-                            placeholder="Seu nome completo" 
-                            required
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                        <input 
-                            type="email" 
-                            className="bg-gray-900 rounded px-5 h-14" 
-                            placeholder="Digite seu e-mail" 
-                            required
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <button 
-                            type="submit"
-                            disabled={loading} 
-                            className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50">
-                                Garantir minha vaga
-                        </button>
-                    </form>
-                </div>
-            </div>
 
-            <img src={CodeMockupImg} className="mt-10" alt="Codígo" />
-        </main>
-        <Footer />
+                <img src={CodeMockupImg} className="mt-10" alt="Codígo" />
+            </main>
+            <Footer />
         </>
-
     )
 }
